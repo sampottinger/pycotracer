9c03c418cd0d8ab79179c327f8962d4fb4950b10
A. Samuel Pottinger <Samnsparky@gmail.com>
2013-07-30 10:36:39 -0600

    Added txt README.

--- setup.py
+++ setup.py
@@ -4,6 +4,14 @@
 @license: GNU GPL v3
 """
 
+# Utility function to read the README file.
+# Used for the long_description.  It's nice, because now 1) we have a top level
+# README file and 2) it's easier to type in the README file than to put a raw
+# string in below ...
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
 from distutils.core import setup
 setup(
     name='pycotracer',
