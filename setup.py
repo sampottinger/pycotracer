c4d953afd7999902660183088fcbb0c7af7791fd
A. Samuel Pottinger <Samnsparky@gmail.com>
2013-09-11 21:10:55 -0600

    Changed version number.

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ def read(fname):
 
 setup(
     name='pycotracer',
-    version='0.4.1',
+    version='0.4.2',
     packages=['pycotracer'],
     author='A. Samuel Pottinger',
     url='https://github.com/Samnsparky/pycotracer',
